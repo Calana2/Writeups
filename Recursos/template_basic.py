79cd559542c3dbe9363c154f7cbf25d10ad7d8cc
Calana2 <kalcast00@proton.me>
2025-08-07 10:13:16 -0400

    Update template_basic.py

--- Recursos/template_basic.py
+++ Recursos/template_basic.py
@@ -8,16 +8,17 @@ def p64(val): return pack("<Q",val)
 def u32(val): return unpack("<I",val)
 def u64(val): return unpack("<Q",val)
 
+bin = "binary"
 domain = "domain.com"
 port = 4444
 
 def start():
-    if sys.argv[1] == "REMOTE":
+    if len(sys.argv) > 1 and sys.argv[1] == "REMOTE":
         s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
         s.connect((domain,port))
         return s
     else:
-        return subprocess.Popen(["./bof"],
+        return subprocess.Popen([bin],
          stdin=subprocess.PIPE,
          stdout=subprocess.PIPE,
          stderr=subprocess.PIPE,)
