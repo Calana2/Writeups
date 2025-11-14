7896ff58182a6278e5b5992c82a52461dbfca22f
Calana2 <kalcast00@proton.me>
2025-11-13 22:11:58 -0500

    Update s.py

--- pwnable.kr/Rookiss/loveletter/s.py
+++ pwnable.kr/Rookiss/loveletter/s.py
@@ -5,8 +5,8 @@ elf = ELF("./loveletter")
 #libc = ELF("./")
 #ld = ELF("./")
 
-contpayloadt.binary = elf
-contpayloadt.terminal = ['tmux', 'splitw', '-hp', '70']
+context.binary = elf
+context.terminal = ['tmux', 'splitw', '-hp', '70']
 #contpayloadt.log_level = "debug"
 gs = '''
 break main
